2197a16fa2976a873eecd30b6acf7b9896d3d5ee
Diego Djordjevic <djordjevic.diego@gmail.com>
2025-05-05 02:56:03 -0500

    added routes for activity logging.

--- pill-pal/src/api/index.js
+++ pill-pal/src/api/index.js
@@ -4,6 +4,7 @@ const dashboardRoutes = require('./dashboard');
 const patientRoutes = require('./patients');
 const medicationsRoutes = require('./medications');
 const providerRoutes = require('./providers');
+const activityLoggerRoutes = require('./activitylogger');
 
 const router = express.Router();
 
@@ -13,5 +14,6 @@ router.use('/dashboard', dashboardRoutes);
 router.use('/patients', patientRoutes);  // Mount patient routes at /patients
 router.use('/medications', medicationsRoutes);
 router.use('/providers', providerRoutes);  // Mount provider routes at /providers
+router.use('/activitylogger', activityLoggerRoutes);  // Mount activity logger routes at /activitylogger
 
 module.exports = router;
